a21bb99be8d3c40859dc4378d743187fc9cfe72e
Dzvelocity <dzikrimurtadlo@gmail.com>
2024-03-03 16:55:29 +0700

    update app proj

--- app/src/main/java/com/myappproj/healthapp/MainActivity.kt
+++ app/src/main/java/com/myappproj/healthapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.myappproj.healthapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Build;
 import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
     }
 }
